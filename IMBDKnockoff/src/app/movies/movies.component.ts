d0cc96fe36e428755765cd377151b0d109d38932
Eduardo Gouveia Alves <ealves@ipn.pt>
2022-07-16 11:38:32 +0100

    WIP

--- IMBDKnockoff/src/app/movies/movies.component.ts
+++ IMBDKnockoff/src/app/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../interfaces/movie';
+import { ServerService } from '../services/server.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
+    public searchDetails: Movie[] = [];
+    public favourites: Movie[] = [];
+    public searchValue: string = '';
+    public displayedColumns: string[] = ['title', 'year', 'commands'];
 
-  constructor() { }
+    constructor(public server: ServerService<Movie[]>) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    public search(searchValue: string): void {
+        this.server.getMovie(searchValue).subscribe((data: any) => {
+            this.searchDetails = data.Search;
+            console.log(this.searchDetails);
+        });
+    }
+
+    public addRemoveFavourite(movie: Movie): void {
+        if (this.favourites.includes(movie)) {
+            this.favourites = this.favourites.filter((item) => item !== movie);
+            return;
+        }
+
+        this.favourites.push(movie);
+    }
 }
---
+++ IMBDKnockoff/src/app/services/server.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ServerService<T> {
+    private serverUrl = 'http://www.omdbapi.com/?apikey=dfffb825&';
+
+    constructor(private http: HttpClient) {}
+
+    public getMovie(search: string): Observable<T> {
+        return this.http.get<T>(`${this.serverUrl}s=${search}&type=movie`);
+    }
+}
