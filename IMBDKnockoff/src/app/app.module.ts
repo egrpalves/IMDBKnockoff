baa0c19026f5682974515890b273ebce234fdff2
Eduardo Gouveia Alves <ealves@ipn.pt>
2022-07-16 09:47:47 +0100

    Add base routes

--- IMBDKnockoff/src/app/app.module.ts
+++ IMBDKnockoff/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, MoviesComponent, MovieDetailsComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
