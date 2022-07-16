72627acbd6ca74db11ef470df88ebdc9de155e9a
Eduardo Gouveia Alves <ealves@ipn.pt>
2022-07-16 09:50:34 +0100

    Add bootstrap

--- IMBDKnockoff/src/app/app.module.ts
+++ IMBDKnockoff/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [AppComponent, MoviesComponent, MovieDetailsComponent],
@@ -22,6 +23,7 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
                 deps: [HttpClient],
             },
         }),
+        NgbModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
