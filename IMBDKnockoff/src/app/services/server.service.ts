d4590a5f56ec5b75f75685e057f709a9b1c87715
Eduardo Gouveia Alves <ealves@ipn.pt>
2022-07-17 18:50:32 +0100

    Add section

--- IMBDKnockoff/src/app/services/server.service.ts
+++ IMBDKnockoff/src/app/services/server.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
+import { Movie } from '../interfaces/movie';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ServerService<T> {
+export class ServerService {
     private serverUrl = 'http://www.omdbapi.com/?apikey=dfffb825&';
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private snackbar: MatSnackBar) {}
 
-    public getMovie(search: string): Observable<T> {
-        return this.http.get<T>(`${this.serverUrl}s=${search}&type=movie`);
+    getMovie(search: string): Observable<Movie[]> {
+        return this.http.get<Movie[]>(`${this.serverUrl}s=${search}&type=movie`);
     }
 
-    public getMovieDetails(id: string): Observable<T> {
-        return this.http.get<T>(`${this.serverUrl}i=${id}&plot=full`);
+    getMovieDetails(id: string): Observable<Movie> {
+        return this.http.get<Movie>(`${this.serverUrl}i=${id}&plot=full`);
+    }
+
+    error(): void {
+        this.snackbar.open('An error has occured', '', {
+            duration: 3000,
+        });
     }
 }
